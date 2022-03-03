9cb7b04a7f339c696272089b5b6cef1c973d7cc6
Krisna Satriadi <krisna.s.362@gmail.com>
2022-03-03 15:09:42 +0700

    add password checker

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,42 +2,47 @@ const { userSignUp, checkUsername } = require('../model/user.model.js'),
     bcrypt = require('bcryptjs');
 
 const addNewUser = async (req, res) => {
-    if (req.body.username && req.body.email && req.body.password) {
-        const passwordhash = await bcrypt.hash(req.body.password, 10);
+    if (req.body.username && req.body.email && req.body.password && req.body.confirm) {
+        if (req.body.password === req.body.confirm) {
+            const passwordhash = await bcrypt.hash(req.body.password, 10);
 
-        await checkUsername({
-            username: req.body.username,
-            email: req.body.email
-        }, async (err, data) => {
-            if (err) {
-                console.log(err);
-            } else {
-                if (data == null) {
-                    await userSignUp({
-                        username: req.body.username,
-                        email: req.body.email,
-                        password: passwordhash
-                    }, (err) => {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            res.send({
-                                status: 'User Added',
-                                username: req.body.username,
-                                email:req.body.email
-                            });
-                        }
-                    });
+            await checkUsername({
+                username: req.body.username,
+                email: req.body.email
+            }, async (err, data) => {
+                if (err) {
+                    console.log(err);
                 } else {
-                    res.send({
-                        status: 'Username or email already exist',
-                        username: data.username,
-                        email: data.email
-                    });
+                    if (data == null) {
+                        await userSignUp({
+                            username: req.body.username,
+                            email: req.body.email,
+                            password: passwordhash
+                        }, (err) => {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                res.send({
+                                    status: 'User Added',
+                                    username: req.body.username,
+                                    email: req.body.email
+                                });
+                            }
+                        });
+                    } else {
+                        res.send({
+                            status: 'Username or email already exist',
+                            username: data.username,
+                            email: data.email
+                        });
+                    }
                 }
-            }
-        });
-
+            });
+        } else {
+            res.send({
+                status: 'password not match'
+            });
+        }
     } else {
         res.send({
             status: 'failed add user'
